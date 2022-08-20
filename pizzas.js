012f51d9f3fb23d1bc5cc2a4f7d0cca867c90ccc
Thalyson Mateus <talisonmateusss@gmail.com>
2022-08-19 22:42:54 -0300

    first commit

---
+++ pizzas.js
@@ -0,0 +1,29 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', 
+    price:[9.50, 17.50, 20.19], 
+    sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    
+    {id:2, name:'Calabresa', img:'images/pizza2.png', 
+    price:[11.99, 14.99, 18.00], 
+    sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', 
+    price:[10.99, 13.99, 17.45], 
+    sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    
+    {id:4, name:'Americana', img:'images/pizza4.png', 
+    price:[7.99, 14.99, 19.77], 
+    sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    
+    {id:5, name:'Sorvete', img:'images/pizza5.png', 
+    price:[12.99, 17.99, 21.43], 
+    sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', 
+    price:[9.99, 13.99, 18.55],
+    sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    
+    {id:7, name:'Chocolate', img:'images/pizza7.png', 
+    price:[11.99, 16.99, 22.36], 
+    sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
